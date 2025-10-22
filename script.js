7bd5977f08cd1f6bae3a269beeb3f35e370275b4
THE RECKONER <162774479+reck07@users.noreply.github.com>
2025-10-22 18:01:37 +0530

    Add files via upload
    An all-in-one organizer, this application is designed for the modern multi-tasker who needs more than just a simple checklist to stay on top of things. This is where productivity meets creativity, allowing you to not only manage your tasks, but also to visually document your progress with an integrated photo gallery. By combining a traditional to-do list with other essential features like note-taking and collaborative tools, it provides a comprehensive yet elegant solution for capturing ideas, organizing projects, and bringing your vision to life in one seamless experience.

---
+++ script.js
@@ -0,0 +1,67 @@
+
+// const wrapper = document.querySelector('.cards-wrapper');
+// let scrollAmount = 0;
+
+// setInterval(()=>{
+//     scrollAmount += 5;
+//     if (scrollAmount >= wrapper.scrollWidth - wrapper.clientWidth){
+//         scrollAmount = 0; //reset and start
+//     }
+//     wrapper.scrollTo({
+//         left: scrollAmount,
+//         behavior: 'smooth'
+//     });
+// }, 60); //adjust speed
+
+
+// declaration input to userInput
+const userInput = document.getElementById("userInput");
+// for display the list on the page
+const tasklist = document.getElementById("tasklist");
+const count = document.getElementById('count');
+// key used in localStorage
+const Storage_key = 'simple_todo_v1';
+
+// taking the input value and create the
+//  new li element and store input content to li
+function addList(){
+  if(userInput.value === ""){
+    alert("input box is empty");
+  } else{
+    let li = document.createElement("li");
+    li.innerHTML = userInput.value;
+    tasklist.appendChild(li);
+
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  userInput.value = "";
+  saveData();
+  updateCount();
+}
+
+tasklist.addEventListener("click", function(e) {
+  // console.log('clicked', e.target, 'tag:', e.target.tagName);
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  }
+  else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveData();
+  }
+}, false);
+
+function updateCount() {
+  let total = tasklist.getElementsByTagName("li").length;
+  count.textContent = total > 0 ? `${total} task(s)` : "No tasks";
+}
+
+function saveData(){
+  localStorage.setItem("data", tasklist.innerHTML)
+}
+function showTask(){
+  tasklist.innerHTML = localStorage.getItem("data");
+}
+showTask()
